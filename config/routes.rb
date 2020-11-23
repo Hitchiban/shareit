8b84990ebc5eff9be034aa0fb8772f627c570b4b
Arthur Deleval <arthur.deleval@skema.edu>
2020-11-23 16:34:36 +0100

    routes created for devices & rents

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :devices, only [:index, :new, :show, :create] do
+    resources :rents, only [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
