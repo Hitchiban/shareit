3dafdc1f6fb33f186cfaaa601cac5e689675b0d6
ValentinLanglet <valentin.langlet59@gmail.com>
2020-11-24 14:03:16 +0100

    footer OK
    Signed-off-by: ValentinLanglet <valentin.langlet59@gmail.com>

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,20 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+
+
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-lewagon');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-lewagon-white');
+      } else {
+        navbar.classList.remove('navbar-lewagon-white');
+      }
+    });
+  }
+}
+
+export { initUpdateNavbarOnScroll };
