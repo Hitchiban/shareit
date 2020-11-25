cd564ec9422a9fb5f12b3512faa7164813c2faad
Hicham Elbaamrani <elbaamrani.hicham@gmail.com>
2020-11-25 21:46:58 +0100

    added review feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_105146) do
+ActiveRecord::Schema.define(version: 2020_11_25_200251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,15 @@ ActiveRecord::Schema.define(version: 2020_11_25_105146) do
     t.index ["user_id"], name: "index_rents_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "device_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["device_id"], name: "index_reviews_on_device_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -82,4 +91,5 @@ ActiveRecord::Schema.define(version: 2020_11_25_105146) do
   add_foreign_key "devices", "users"
   add_foreign_key "rents", "devices"
   add_foreign_key "rents", "users"
+  add_foreign_key "reviews", "devices"
 end
