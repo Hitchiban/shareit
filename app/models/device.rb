7910513a8fe3b3c931035a3cbb4d4e917a8511e5
Arthur Deleval <arthur.deleval@skema.edu>
2020-11-26 17:36:03 +0100

    cloudinary on the show done

--- app/models/device.rb
+++ app/models/device.rb
@@ -3,14 +3,14 @@ class Device < ApplicationRecord
   validates :name, :description, :address, :price, presence: true
   has_many :rents, dependent: :destroy
   has_many_attached :photos
-
+  has_many :reviews, dependent: :destroy
+  
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  has_many :reviews, dependent: :destroy
   include PgSearch::Model
   pg_search_scope :global_search,
-  against: [[:name, 'A' ], [:description, 'B' ]],
+  against: [[:address, 'A' ], [:name, 'B' ], [:description, 'C' ]],
   associated_against: {
     user: [ :first_name, :last_name, :nickname ]
   },
