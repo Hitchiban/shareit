999d38dc1b4f1b68ad66add2a2c47ca6bffa0387
Arthur Deleval <arthur.deleval@skema.edu>
2020-11-27 16:13:49 +0100

    frond show

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :device
   validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
   validates :content, presence: true
+  validates :title, presence: true
 end
